9ebb69f315ac76ec7c25c971b58382bca52f8059
Omkar Khoche <omkarkhoche3448@gmail.com>
2025-02-12 07:59:17 +0530

    Added Header.svelte, updated routes and config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Header from './Header.svelte';
+
+	// svelte-ignore non_reactive_update
+	// let name ='Omkar';
+	let name = $state('Omkar');
+	let status = $state('OPEN');
+
+	function buttonClickHandler() {
+		status = status === 'OPEN' ? 'CLOSE' : 'OPEN';
+	}
+</script>
+
+<Header prop={name} />
+
+<input type="text" bind:value={name} />
+
+<p>This Store is now {status}</p>
+<button onclick={buttonClickHandler}>Toggle status</button>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+	let { prop } = $props();
+</script>
+
+<h1>Hello i am {prop}</h1>
+
+<p>{prop}</p>
+
+<style>
+	h1 {
+		color: blueviolet;
+	}
+</style>
