3367246a726f0136e7a37b7ee398f34cc510e0f5
Omkar Khoche <omkarkhoche3448@gmail.com>
2025-02-12 23:36:48 +0530

    implement inline CSS styles for children components using snippets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	// import Header from './Header.svelte';
+	import Header from './Header.svelte';
 
 	// svelte-ignore non_reactive_update
 	// let name ='Omkar';
@@ -23,16 +23,23 @@
 		if (formState.step === 0 && formState.name.trim() === '') {
 			formState.error = 'Your Name is Empty. Please enter your name.';
 			return;
-		} 
+		}
 		if (formState.step === 1 && formState.birthday.trim() === '') {
 			formState.error = 'Please enter your birthday.';
 			return;
 		}
-		
+
 		formState.error = '';
 		formState.step++;
 	}
 </script>
+
+<Header prop={formState.name}>
+	<h1>i am Child 1 of Header</h1>
+	{#snippet secondChild(name)}
+		<p>secondChild name is : {name}</p>
+	{/snippet}
+</Header>
 
 <!-- 
 <Header prop={name} />
@@ -45,24 +52,49 @@
 <button onclick={buttonClickHandler}>Toggle status</button> 
 -->
 
-<p>Step:{formState.step+1}</p>
+<p>Step:{formState.step + 1}</p>
 
-{#if formState.error}
-	<p class="error">{formState.error}</p>
-{/if}
-
-{#if formState.step === 0}
+<!-- {#if formState.step === 0}
 	<div>
 		<label for="name">Your Name: </label>
 		<input type="text" id="name" bind:value={formState.name} />
 	</div>
 	<button onclick={buttonClickHandler}>next</button>
-
-	{:else if formState.step === 1}
+{:else if formState.step === 1}
 	<div>
 		<label for="birthday">Your birthday: </label>
 		<input type="date" id="birthday" bind:value={formState.birthday} />
 	</div>
 	<button onclick={buttonClickHandler}>next</button>
-	
 {/if}
+
+{#if formState.error}
+	<p class="error">{formState.error}</p>
+{/if} -->
+
+{#if formState.step === 0}
+{@render formStep({Question:"What is Your Name ",id:"name", type:"text"})}
+{/if}
+
+{#if formState.step === 1}
+{@render formStep({Question:"What is Your Age ",id:"birthday", type:"date"})}
+{/if}
+
+{#if formState.error}
+	<p class="error">{formState.error}</p>
+{/if} 
+
+{#snippet formStep({ id, Question, type })}
+	<div>
+		<label for={id}>{Question}</label>
+		<input type={type} id={id} bind:value={formState[id]} />
+	</div>
+	<button onclick={buttonClickHandler}> next</button>
+{/snippet}
+
+
+<style>
+	:global(.error) {
+		color: red;
+	}
+</style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,10 +1,22 @@
-<script>
-	let { prop } = $props();
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	let { prop ,children,secondChild}:{
+		prop:String,
+		children:Snippet,
+		secondChild:Snippet
+	}= $props();
 </script>
 
-<h1>Hello i am {prop}</h1>
+<div>
+	<!-- <h1>Hello i am {prop}</h1> -->
+	<!-- <p>{prop}</p> -->
+	 <h1>{prop ? prop : 'User'}'s Form</h1>
+	 {@render children()}
 
-<p>{prop}</p>
+	<h3> {@render secondChild('I am Sec Child')}</h3>
+
+</div>
 
 <style>
 	h1 {
