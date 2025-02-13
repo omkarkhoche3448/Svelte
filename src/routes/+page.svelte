84f13d6bba4c8b62543783489101fc9c66bcbc49
Omkar Khoche <omkarkhoche3448@gmail.com>
2025-02-13 16:42:06 +0530

    implement each loop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	// @ts-nocheck
+
 	import Header from './Header.svelte';
 
 	// svelte-ignore non_reactive_update
@@ -12,29 +14,56 @@
 	// 	status = status === 'OPEN' ? 'CLOSE' : 'OPEN';
 	// }
 
+	// let formState = $state({
+	// 	name: '',
+	// 	birthday: '',
+	// 	step: 0,
+	// 	error: ''
+	// });
+
+	//function buttonClickHandler() {
+	//	if (formState.step === 0 && formState.name.trim() === '') {
+	// formState.error = 'Your Name is Empty. Please enter your name.';
+	// return;
+	// }
+	// if (formState.step === 1 && formState.birthday.trim() === '') {
+	// formState.error = 'Please enter your birthday.';
+	// return;
+	// }
+	//
+	// formState.error = '';
+	// formState.step++;
+	// }
+
 	let formState = $state({
-		name: '',
-		birthday: '',
+		answer: {},
 		step: 0,
 		error: ''
 	});
-
-	function buttonClickHandler() {
-		if (formState.step === 0 && formState.name.trim() === '') {
-			formState.error = 'Your Name is Empty. Please enter your name.';
-			return;
+	const QUESTIONS = [
+		{
+			question: 'What is Your name',
+			id: 'name',
+			type: 'text'
+		},
+		{
+			question: 'What is Your name',
+			id: 'birthday',
+			type: 'date'
 		}
-		if (formState.step === 1 && formState.birthday.trim() === '') {
-			formState.error = 'Please enter your birthday.';
-			return;
-		}
+	];
 
-		formState.error = '';
-		formState.step++;
+	function nextStep(id) {
+		if (formState.answer[id] && formState.answer[id].trim() !== '') {
+			formState.error = '';
+			formState.step++;
+		} else {
+			formState.error = 'Please fill out the field.';
+		}
 	}
 </script>
 
-<Header prop={formState.name}>
+<Header prop={formState.answer.name}>
 	<h1>i am Child 1 of Header</h1>
 	{#snippet secondChild(name)}
 		<p>secondChild name is : {name}</p>
@@ -47,14 +76,14 @@
 <p>{full_name}</p>
 
 <input type="text" bind:value={name} />
-
 <p>This Store is now {status}</p>
 <button onclick={buttonClickHandler}>Toggle status</button> 
+
 -->
-
-<p>Step:{formState.step + 1}</p>
-
-<!-- {#if formState.step === 0}
+<main>
+	<!-- <p>Step:{formState.step + 1}</p> -->
+	<!-- 
+{#if formState.step === 0}
 	<div>
 		<label for="name">Your Name: </label>
 		<input type="text" id="name" bind:value={formState.name} />
@@ -70,8 +99,10 @@
 
 {#if formState.error}
 	<p class="error">{formState.error}</p>
-{/if} -->
+{/if} 
+-->
 
+	<!-- 
 {#if formState.step === 0}
 {@render formStep({Question:"What is Your Name ",id:"name", type:"text"})}
 {/if}
@@ -82,7 +113,7 @@
 
 {#if formState.error}
 	<p class="error">{formState.error}</p>
-{/if} 
+{/if}  
 
 {#snippet formStep({ id, Question, type })}
 	<div>
@@ -91,7 +122,33 @@
 	</div>
 	<button onclick={buttonClickHandler}> next</button>
 {/snippet}
+-->
+	{#if formState.step >= QUESTIONS.length}
+		<p>Thank You!</p>
+	{:else}
+		<p>Step:{formState.step + 1}</p>
+	{/if}
 
+	{#each QUESTIONS as question, index (question.id)}
+		{#if formState.step === index}
+			{@render formStep(question)}
+		{/if}
+	{/each}
+
+	{#if formState.error}
+		<p class="error">{formState.error}</p>
+	{/if}
+
+	{#snippet formStep({ id, question, type })}
+		<div>
+			<label for={id}>{question}</label>
+			<input {type} {id} bind:value={formState.answer[id]} />
+		</div>
+		<button onclick={() => nextStep(id)}>Next</button>
+	{/snippet}
+
+	<!-- {JSON.stringify(formState)} -->
+</main>
 
 <style>
 	:global(.error) {
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -12,9 +12,9 @@
 	<!-- <h1>Hello i am {prop}</h1> -->
 	<!-- <p>{prop}</p> -->
 	 <h1>{prop ? prop : 'User'}'s Form</h1>
-	 {@render children()}
+	 <!-- {@render children()}
 
-	<h3> {@render secondChild('I am Sec Child')}</h3>
+	<h3> {@render secondChild('I am Sec Child')}</h3> -->
 
 </div>
 
